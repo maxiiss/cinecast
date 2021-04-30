bf8f51a769a5d3cad9b079787543cfc5aa099d78
maxiiss <maxilairee@gmail.com>
2021-04-30 13:34:53 +0200

    Alert feature

--- web-app/src/views/Home.vue
+++ web-app/src/views/Home.vue
@@ -1,65 +1,85 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <v-card :loading="dev_loading" :disabled="dev_loading" outlined>
-          <v-card-title> Home Cinema Configuration </v-card-title>
-          <v-card-text>
-            <v-form v-model="valid1" ref="form1">
-              <v-row>
-                <v-col cols="12">
-                  <v-autocomplete
-                    v-model="v_device"
-                    :items="video_devices"
-                    :rules="[(v) => !!v || 'Video device must be chosen']"
-                    item-text="friendly_name"
-                    label="Video Device"
-                    return-object
-                  ></v-autocomplete>
-                </v-col>
-                <v-col cols="12">
-                  <v-autocomplete
-                    v-model="a_device"
-                    :items="audio_devices"
-                    :rules="[(v) => !!v || 'Audio device must be chosen']"
-                    item-text="friendly_name"
-                    label="Audio Device"
-                    return-object
-                  ></v-autocomplete>
-                </v-col>
-              </v-row>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text @click="setConfig()" color="accent">Validate</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-card :disabled="!configValidated" outlined>
-          <v-card-title> Media </v-card-title>
-          <v-card-text>
-            <v-form>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field label="Video url" v-model="url"></v-text-field>
-                </v-col>
-              </v-row>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text @click="playUrl()" color="accent"
-              ><v-icon>mdi-play</v-icon></v-btn
+  <div id="home">
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-card flat height="50px">
+            <v-alert
+              :type="not_type"
+              dismissible
+              transition="slide-y-transition"
+              text
+              v-model="notification"
             >
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+              {{ not_message }}</v-alert
+            >
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-card :loading="dev_loading" :disabled="dev_loading" outlined>
+            <v-card-title> Home Cinema Configuration </v-card-title>
+            <v-card-text>
+              <v-form v-model="valid1" ref="form1">
+                <v-row>
+                  <v-col cols="12">
+                    <v-autocomplete
+                      v-model="v_device"
+                      :items="video_devices"
+                      :rules="[(v) => !!v || 'Video device must be chosen']"
+                      item-text="friendly_name"
+                      label="Video Device"
+                      return-object
+                    ></v-autocomplete>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-autocomplete
+                      v-model="a_device"
+                      :items="audio_devices"
+                      :rules="[(v) => !!v || 'Audio device must be chosen']"
+                      item-text="friendly_name"
+                      label="Audio Device"
+                      return-object
+                    ></v-autocomplete>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click="setConfig()" color="accent">Validate</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-card :disabled="!configValidated" outlined>
+            <v-card-title> Media </v-card-title>
+            <v-card-text>
+              <v-form>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Video url"
+                      v-model="url"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click="playUrl()" color="accent"
+                ><v-icon>mdi-play</v-icon></v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -76,6 +96,9 @@ export default {
     url: "",
     valid1: false,
     configValidated: false,
+    notification: false,
+    not_message: "",
+    not_type: "warning",
   }),
   computed: {
     video_devices: function () {
@@ -90,21 +113,27 @@ export default {
       this.url =
         "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
     }
-    axios
-      .get(`http://127.0.0.1:5000/api/devices`)
-      .then((response) => {
-        // JSON responses are automatically parsed.
-        console.log(response);
-        this.devices = response.data.devices;
-        this.dev_loading = false;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    this.getDevices();
   },
   methods: {
     openPlayer: function () {
       this.$emit("openPlayer", true);
+    },
+    getDevices: function () {
+      axios
+        .get(`http://127.0.0.1:5000/api/devices`)
+        .then((response) => {
+          console.log(response);
+          this.devices = response.data.devices;
+          this.dev_loading = false;
+        })
+        .catch((e) => {
+          console.error(e);
+          this.not_message =
+            "Unable to reach the backend. Please refresh the page";
+          this.not_type = "error";
+          this.notification = true;
+        });
     },
     playUrl: function () {
       this.openPlayer();
@@ -136,6 +165,10 @@ export default {
           })
           .catch((e) => {
             console.log(e);
+            this.not_message =
+              "Unable to control at least one device. You may try to restart the devices.";
+            this.not_type = "error";
+            this.notification = true;
             this.configValidated = false;
             this.$refs.form1.reset();
           });
@@ -144,3 +177,14 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#home {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
